f3bfdc1e7a96365649fa81b05c608e28a5fe52c9
Aerfog <danil.pochepinecc@gmail.com>
2023-05-13 01:56:59 +0300

    Complete?

--- Graph.cs
+++ Graph.cs
@@ -1,61 +1,110 @@
 ﻿namespace Lab12
 {
-    internal class Graph
+    public class Edge
     {
-        private int[,] list;
-        private int n;
-        public Graph(int n)
+        public readonly Node From;
+        public readonly Node To;
+        public Edge(Node first, Node second)
         {
-            list = new int[n, n];
-            this.n = n;
+            this.From = first;
+            this.To = second;
+        }
+        public bool IsIncident(Node node)
+        {
+            return From == node || To == node;
+        }
+        public Node OtherNode(Node node)
+        {
+            if (!IsIncident(node)) throw new ArgumentException();
+            if (From == node) return To;
+            return From;
+        }
+    }
+
+    public class Node
+    {
+        readonly List<Edge> edges = new List<Edge>();
+        public readonly int NodeNumber;
+
+        public Node(int number)
+        {
+            NodeNumber = number;
         }
 
-        public Graph(int[,] arr)
+        public IEnumerable<Node> IncidentNodes
         {
-            list = arr;
-            this.n = arr.GetLength(0);
-        }
-        public void Add(int from, int to, int cost)
-        {
-            list[from, to] = cost;
-        }
-
-        public IList<int> DijkstraAlgoritme(int start)
-        {
-            List<int> distance = new List<int>();
-            bool[] visited = new bool[n];
-            for(int i =0;i<n;i++)
+            get
             {
-                distance.Add(int.MaxValue);
-                visited[i] = false;
+                return edges.Select(z => z.OtherNode(this));
             }
-
-            distance[start] = 0;
-            int index = -1;
-            for(int i = 0; i< n;i++)
+        }
+        public IEnumerable<Edge> IncidentEdges
+        {
+            get
             {
-                int min = int.MaxValue;
-                for(int j = 0; j < n; j++)
-                {
-                    if (!visited[j] && distance[j] <= min)
-                    {
-                        min = distance[j];
-                        index = j; 
-
-                    }
-                }
-                visited[index] = true;
-                for(int j =0;j<n; j++)
-                {
-                    if (!visited[j] &&
-                        list[index, j] > -1 &&
-                        distance[index] != int.MaxValue &&
-                        distance[index] + list[index, j] < distance[j])
-                            distance[j] = distance[index] + list[index, j];
-                }
+                foreach (var e in edges) yield return e;
             }
+        }
+        public static Edge Connect(Node node1, Node node2, Graph graph)
+        {
+            if (!graph.Nodes.Contains(node1) || !graph.Nodes.Contains(node2)) throw new ArgumentException();
+            var edge = new Edge(node1, node2);
+            node1.edges.Add(edge);
+            node2.edges.Add(edge);
+            return edge;
+        }
+        public static void Disconnect(Edge edge)
+        {
+            edge.From.edges.Remove(edge);
+            edge.To.edges.Remove(edge);
+        }
+    }
 
-            return distance;
+    public class Graph
+    {
+        private Node[] nodes;
+
+        public Graph(int nodesCount)
+        {
+            nodes = Enumerable.Range(0, nodesCount).Select(z => new Node(z)).ToArray();
+        }
+
+        public int Length { get { return nodes.Length; } }
+
+        public Node this[int index] { get { return nodes[index]; } }
+
+        public IEnumerable<Node> Nodes
+        {
+            get
+            {
+                foreach (var node in nodes) yield return node;
+            }
+        }
+
+        public Edge Connect(int index1, int index2)
+        {
+            return Node.Connect(nodes[index1], nodes[index2], this);
+        }
+
+        public void Delete(Edge edge)
+        {
+            Node.Disconnect(edge);
+        }
+
+        public IEnumerable<Edge> Edges
+        {
+            get
+            {
+                return nodes.SelectMany(z => z.IncidentEdges).Distinct();
+            }
+        }
+
+        public static Graph MakeGraph(params int[] incidentNodes)
+        {
+            var graph = new Graph(incidentNodes.Max() + 1);
+            for (int i = 0; i < incidentNodes.Length - 1; i += 2)
+                graph.Connect(incidentNodes[i], incidentNodes[i + 1]);
+            return graph;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,75 @@
-﻿namespace Lab12
+﻿using System.Xml.Linq;
+
+namespace Lab12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[,] table =
-            {   //0  1   2    3   4   5
-           /*0*/{ 0, 18, 25, -1, -1, -1},
-           /*1*/{ -1, 0, -1, -1, -1, -1},
-           /*2*/{ -1, -1, 0, 5, 23, 30},
-           /*3*/{ -1, -1, -1, 0, -1, -1},
-           /*4*/{ -1, -1, -1, -1, 0, 10},
-           /*5*/{ -1, -1, -1, -1, -1, 0}
-            };
+            var graph = new Graph(6);
+            var weights = new Dictionary<Edge, double>();
+            weights[graph.Connect(0, 1)] = 18;
+            weights[graph.Connect(0, 2)] = 25;
+            weights[graph.Connect(2, 3)] = 5;
+            weights[graph.Connect(2, 4)] = 23;
+            weights[graph.Connect(2, 5)] = 30;
+            weights[graph.Connect(4, 5)] = 10;
 
-            Graph graph = new Graph(table);
-            int start = 0;
-            var dist = graph.DijkstraAlgoritme(start);
-            int i = 1;
-            foreach (var item in dist)
+            var path = Dijkstra(graph, weights, graph[0], graph[5]).Select(n => n.NodeNumber+1);
+            foreach (var node in path)
             {
-                Console.WriteLine("From {0} to {1} -> {2}", start+1,i,item);
-                i++;
+                Console.WriteLine(node);
             }
         }
+        public static List<Node> Dijkstra(Graph graph, Dictionary<Edge, double> weights, Node start, Node end)
+        {
+            var notVisited = graph.Nodes.ToList();
+            var track = new Dictionary<Node, DijkstraData>();
+            track[start] = new DijkstraData { Price = 0, Previous = null };
+
+            while (true)
+            {
+                Node toOpen = null;
+                var bestPrice = double.PositiveInfinity;
+                foreach (var e in notVisited)
+                {
+                    if (track.ContainsKey(e) && track[e].Price < bestPrice)
+                    {
+                        bestPrice = track[e].Price;
+                        toOpen = e;
+                    }
+                }
+
+                if (toOpen == null) return null;
+                if (toOpen == end) break;
+
+                foreach (var e in toOpen.IncidentEdges.Where(z => z.From == toOpen))
+                {
+                    var currentPrice = track[toOpen].Price + weights[e];
+                    var nextNode = e.OtherNode(toOpen);
+                    if (!track.ContainsKey(nextNode) || track[nextNode].Price > currentPrice)
+                    {
+                        track[nextNode] = new DijkstraData { Previous = toOpen, Price = currentPrice };
+                    }
+                }
+
+                notVisited.Remove(toOpen);
+            }
+
+            var result = new List<Node>();
+            while (end != null)
+            {
+                result.Add(end);
+                end = track[end].Previous;
+            }
+            result.Reverse();
+            return result;
+        }
+    }
+
+    class DijkstraData
+    {
+        public Node Previous { get; set; }
+        public double Price { get; set; }
     }
 }
