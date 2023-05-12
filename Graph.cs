289588ecd40e2754cec9cc07b9ea2d82a281b5bc
Aerfog <danil.pochepinecc@gmail.com>
2023-05-13 01:26:37 +0300

    Complete Task

---
+++ Graph.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab12
+{
+    internal class Graph
+    {
+        private int[,] list;
+        private int n;
+        public Graph(int n)
+        {
+            list = new int[n, n];
+            this.n = n;
+        }
+
+        public Graph(int[,] arr)
+        {
+            list = arr;
+            this.n = arr.GetLength(0);
+        }
+        public void Add(int from, int to, int cost)
+        {
+            list[from, to] = cost;
+        }
+
+        public IList<int> DijkstraAlgoritme(int start)
+        {
+            List<int> distance = new List<int>();
+            bool[] visited = new bool[n];
+            for(int i =0;i<n;i++)
+            {
+                distance.Add(int.MaxValue);
+                visited[i] = false;
+            }
+
+            distance[start] = 0;
+            int index = -1;
+            for(int i = 0; i< n;i++)
+            {
+                int min = int.MaxValue;
+                for(int j = 0; j < n; j++)
+                {
+                    if (!visited[j] && distance[j] <= min)
+                    {
+                        min = distance[j];
+                        index = j; 
+
+                    }
+                }
+                visited[index] = true;
+                for(int j =0;j<n; j++)
+                {
+                    if (!visited[j] &&
+                        list[index, j] > -1 &&
+                        distance[index] != int.MaxValue &&
+                        distance[index] + list[index, j] < distance[j])
+                            distance[j] = distance[index] + list[index, j];
+                }
+            }
+
+            return distance;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi!");
+            int[,] table =
+            {   //0  1   2    3   4   5
+           /*0*/{ 0, 18, 25, -1, -1, -1},
+           /*1*/{ -1, 0, -1, -1, -1, -1},
+           /*2*/{ -1, -1, 0, 5, 23, 30},
+           /*3*/{ -1, -1, -1, 0, -1, -1},
+           /*4*/{ -1, -1, -1, -1, 0, 10},
+           /*5*/{ -1, -1, -1, -1, -1, 0}
+            };
+
+            Graph graph = new Graph(table);
+            int start = 0;
+            var dist = graph.DijkstraAlgoritme(start);
+            int i = 1;
+            foreach (var item in dist)
+            {
+                Console.WriteLine("From {0} to {1} -> {2}", start+1,i,item);
+                i++;
+            }
         }
     }
 }
